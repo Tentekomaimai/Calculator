58de51c75a04682331ee8061d7eb00a73e305fff
Tentekomaimai <chasojuken@yahoo.co.jp>
2017-05-17 22:22:52 +0900

    Simplified

--- calculator.py
+++ calculator.py
@@ -1,29 +1,13 @@
 # coding: utf-8
 import sys
-try:
-    x = input('式を入力:')
+x = input("式を入力:")
+while x != exit:
     if type(x) == int:
-        print(int(x))
+        print("答えは" + str(int(x)))
+    elif type(x) == float:
+        print("答えは" + str(float(x)))
     else:
-        print(float(x))
-except:
-    print("エラー")
-    sys.exit()
-y = input('計算を続けるには0 止めるには1を入力:')
-while y == 0:
-    try:
-        x = input('式を入力:')
-        if type(x) == int:
-            print(int(x))
-        else:
-            print(float(x))
-    except:
-        print("エラー")
-        sys.exit()
-    y = input('計算を続けるには0 止めるには1を入力:')
-if y == 1:
-    print("終了します")
-    sys.exit()
-else:
-    print("エラー")
-    sys.exit()
+        pass
+    x = input(":")
+print("終了します")
+sys.exit()

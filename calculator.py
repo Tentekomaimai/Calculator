cfea9f6f7fd841220e5b5c5697a29a77fd940a9b
Tentekomaimai <chasojuken@yahoo.co.jp>
2017-05-23 14:26:58 +0900

    Python2.7 to 3.6

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 # coding: utf-8
 import sys
-x = input("式を入力:")
+x = eval(input("式を入力:"))
 while x != exit:
     print("答えは、" + str(x))
-    x = input("式を入力:")
+    x = eval(input("式を入力:"))
 print("終了します")
 sys.exit()
